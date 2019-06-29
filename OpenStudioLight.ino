e3de08a0c4692476ff5b06f10b8f0516b8eba6c6
SATURNO-V\Jorge <100317118@alumnos.uc3m.es>
2019-06-29 17:41:35 +0200

    First code commit.

---
+++ OpenStudioLight.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       OpenStudioLight.ino
+    Created:	29/06/2019 17:40:24
+    Author:     SATURNO-V\Jorge
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
