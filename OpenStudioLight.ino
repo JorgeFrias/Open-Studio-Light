0ef7bbfc44fca1b1fdcffc293cb86a3d080afdca
SATURNO-V\Jorge <100317118@alumnos.uc3m.es>
2019-06-29 18:11:10 +0200

    Reading input from a potenciometer.

--- OpenStudioLight.ino
+++ OpenStudioLight.ino
@@ -1,32 +1,32 @@
-// Visual Micro is in vMicro>General>Tutorial Mode
-// 
 /*
     Name:       OpenStudioLight.ino
     Created:	29/06/2019 17:40:24
-    Author:     SATURNO-V\Jorge
+    Author:     Jorge Frias
 */
 
-// Define User Types below here or use a .h file
+// Define User Types 
+//
+int potPin = A2;    // select the input pin for the potentiometer
+int val = 0;       // variable to store the value coming from the sensor
+
+
+// Function Prototypes
 //
 
 
-// Define Function Prototypes that use User Types below here or use a .h file
-//
-
-
-// Define Functions below here or use other .ino or cpp files
+// Functions 
 //
 
 // The setup() function runs once each time the micro-controller starts
 void setup()
 {
-
-
+  Serial.begin(9600);
 }
 
 // Add the main program code into the continuous loop() function
 void loop()
 {
-
-
+	val = analogRead(potPin);    // read the value from the sensor
+	Serial.println(val);
+	delay(500);
 }
